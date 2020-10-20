43fee14217a568ba9f0f384a95e64f0a2d76e4ba
Luarde <luarde15@gmail.com>
2020-10-20 20:16:51 -0300

    Funções com retorno

---
+++ funcoes_com_retorno.py
@@ -0,0 +1,21 @@
+"""
+Funções com retorno
+
+numero = [1, 2, 3]
+
+retPop = numero.pop()
+
+print(f'Retorno de POP {retPop}')
+
+retPr = print(numero)
+
+print(f'Retorno de Print: {retPr}')
+
+"""
+def quadradoDe7():
+    return 7 * 7
+
+ret = quadradoDe7()
+
+print(f'Retorno {ret}')
+print(f'Retorno {quadradoDe7() + 1}')
