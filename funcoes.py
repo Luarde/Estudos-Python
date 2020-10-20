29a2ce7f3bb001525d1316a2a182dba0e9274e82
Luarde <luarde15@gmail.com>
2020-10-20 18:33:26 -0300

    Treinando básico de funções

---
+++ funcoes.py
@@ -0,0 +1,31 @@
+# Definindo funções
+""" 
+Funções são pequenos trechos de códigos onde realizam tarefas específicas. 
+
+cores = ['verde', 'amarelo', 'azul', 'branco']
+
+print(cores) - o Print é considerado uma função
+
+cores.append('roxo')
+
+print(cores)
+cores.clear()
+
+print (cores)
+
+Definições de funções iniciam com 'def()': 
+""" 
+
+def cantarParabens():
+    print('Parabéns pra você')
+    print('Nessa data querida')
+    print('Muitas felicidades')
+    print('Menos anos de vidaaaaa')
+
+
+# for n in range(4):
+#    cantarParabens()
+
+canta = cantarParabens
+
+canta()
